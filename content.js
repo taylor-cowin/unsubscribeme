80fc0e77e747bbe86f01cc902f764ce58e005812
Taylor Cowin <80193327+taylor-cowin@users.noreply.github.com>
2024-11-04 17:21:29 -0600

    pull my finger
    broken but making progress. Might need to migrate all to background.js

--- content.js
+++ content.js
@@ -1,25 +1,32 @@
 // content.js
 
-function findUnsubscribeLinks() {
-    
-  // Find all links on the page
-    var links = window.document.querySelectorAll("a");
-    //DEBUG
-    console.log(links.length + " links acquired: " + links);
-    // Send each unsubscribe link to the background script to open in a new tab
-    for (const link of links){
-      //DEBUG
-      console.log("LINK SET: " + link.href);
-      chrome.runtime.sendMessage({type: 'openTab', url: link.href });
-    }
-    
-    // Send the unsubscribe link data to the popup for feedback
-    chrome.runtime.sendMessage({ type: 'emailList', emails: unsubscribeEmails });
-  }
-  
+function findUnsubscribeLinks () {
   //DEBUGGING
-  console.log("Gmail has been detected -- running content.js");
+  console.log('Gmail has been detected -- running content.js')
 
- // Run findUnsubscribeLinks when the page loads
- //window.addEventListener('load', findUnsubscribeLinks)
- findUnsubscribeLinks(); 
+  // Find all links on the page
+  var links = window.document.querySelectorAll('a')
+  //DEBUG
+  console.log(links.length + ' links acquired: ' + links)
+
+  // Send each unsubscribe link to the background script after checking to see if it's an unsubscribe link
+  for (const link of links) {
+    //"i" after the RegEx makes it case-insensitive
+    if (
+      /unsubscribe/i.test(link.textContent) ||
+      /unsubscribe/i.test(link.href)
+    ) {
+      //DEBUG
+      console.log('LINK SET: ' + link.href)
+      //It says "unsubscribe," so open a tab.
+      chrome.runtime.sendMessage({ type: 'openTab', url: link.href })
+    }
+  }
+}
+/* THIS ISN'T WORKING AND I THINK IT MAY NEED TO MOVE TO BACKGROUND.JS-- DO
+   WE EVEN NEED A CONTENT.JS OR CAN ALL BE HANDLED IN BACKGROUND SCRIPT? */
+
+   // Run findUnsubscribeLinks() after the entire DOM is loaded
+chrome.webNavigation.onDOMContentLoaded.addListener((x) => {
+findUnsubscribeLinks();
+});
